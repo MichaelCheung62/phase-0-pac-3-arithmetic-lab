22997f02e2cb461efc50c36e6cfab7dd70997ea2
Michael <MichaelCheung101@gmail.com>
2021-12-21 20:32:55 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+const add = (num1, num2) => {
+return num1 + num2
+}
+
+const subtract= (num1,num2) => {
+    return num1 - num2
+}
+
+const multiply = (num1,num2) => {
+    return num1 * num2
+}
+
+const divide =(num1,num2) => {
+    return num1 / num2
+}
+
+const increment = (num) => {
+
+    return ++num
+    
+}
+
+const decrement = (num) => {
+    return --num
+}
+
+const makeInt = (num) => {
+    return parseInt(num,10)
+}
+
+const preserveDecimal =(num) => {
+    return parseFloat(num)
+}
